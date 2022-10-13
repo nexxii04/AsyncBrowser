baf10b488e08aed9cf7e08ff55b91e00e9acd82e
NexDev <111369635+NexD4v@users.noreply.github.com>
2022-10-13 22:30:14 +0200

    test format Main.php

--- src/AsyncBrowser/Main.php
+++ src/AsyncBrowser/Main.php
@@ -7,17 +7,17 @@ namespace AsyncBrowser;
 use AsyncBrowser\Request\Queue;
 use AsyncBrowser\Exception\AsyncBrowserException;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginBase; 
 
 class Main extends PluginBase { 
 
-	public function onLoad(): void {
-		$bootstrap = dirname(__DIR__, 2) . '/vendor/autoload.php';
-		if (!is_file($bootstrap)) {
-			throw new AsyncBrowserException('install the composer dependencies');
-		}
+    public function onLoad(): void {
+        $bootstrap = dirname(__DIR__, 2) . '/vendor/autoload.php';
+        if (!is_file($bootstrap)) {
+            throw new AsyncBrowserException('install the composer dependencies');
+        }
 
-		require($bootstrap);
-		new Queue();
-	}
+        require($bootstrap);
+        new Queue();
+    }
 }
